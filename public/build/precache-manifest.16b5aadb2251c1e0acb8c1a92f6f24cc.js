47e0dbadb99fcf213bab8ceb47701d0466bfdfcc
hufflepuffprogrammer <wdrum4food@yahoo.com>
2020-05-08 20:56:35 -0700

    make it better

---
+++ public/build/precache-manifest.16b5aadb2251c1e0acb8c1a92f6f24cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bdb0c9eec8291e4efe23d88613db9b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "452f16859a81aa0dc74a",
+    "url": "/static/css/main.0a15a849.chunk.css"
+  },
+  {
+    "revision": "cd026454bf785aaa4675",
+    "url": "/static/js/2.647f0b05.chunk.js"
+  },
+  {
+    "revision": "3e6933cf6bd44f8f91c7aa725d75c668",
+    "url": "/static/js/2.647f0b05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "452f16859a81aa0dc74a",
+    "url": "/static/js/main.f5ad7761.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
